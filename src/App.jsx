c4efecf5c71736f51086ef21b5dd4208ee5666bc
abdulladone94 <abdulladone94@gmail.com>
2024-11-30 07:08:38 +0530

    product detail page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Layout from "./components/layout";
 import Home from "./pages/Home";
+import DetailProduct from "./pages/DetailProduct";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="/:slug" element={<DetailProduct />} />
         </Route>
       </Routes>
     </BrowserRouter>
