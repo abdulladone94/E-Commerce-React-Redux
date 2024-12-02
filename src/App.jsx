d4cb40f7c6367ce06f7806255cd04088ba49440e
abdulladone94 <abdulladone94@gmail.com>
2024-12-02 11:43:04 +0530

    created privet routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,34 @@ import "./App.css";
 import Layout from "./components/layout";
 import Home from "./pages/Home";
 import DetailProduct from "./pages/DetailProduct";
+import SignUpForm from "./pages/SignUp";
+import LoginForm from "./pages/Login";
+import PrivateRoute from "./components/privateRoute";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="/sign-up" element={<SignUpForm />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <Layout />{" "}
+            </PrivateRoute>
+          }
+        >
           <Route index element={<Home />} />
-          <Route path="/:slug" element={<DetailProduct />} />
+
+          <Route
+            path="/:slug"
+            element={
+              <PrivateRoute>
+                <DetailProduct />
+              </PrivateRoute>
+            }
+          />
         </Route>
       </Routes>
     </BrowserRouter>
